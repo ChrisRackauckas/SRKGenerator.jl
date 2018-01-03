6dba1d9e2db558c9a181bebf3c3230a4521fe493
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-01-03 06:55:06 -0800

    update pathwise stiff picture

--- assets/benchmarking/pathwise_stiffness.jl
+++ assets/benchmarking/pathwise_stiffness.jl
@@ -6,7 +6,9 @@ g(t,u) = 10.0
 prob = SDEProblem(f,g,2.0,(0.0,5.0))
 
 sol = solve(prob,SOSRI(),abstol=1e-2,reltol=1e-2,saveat=0.025,seed=3)
-plot(sol,legend=false,yaxis = L"X_t",title = "Pathwise Stiffness Example")
+plot(sol,legend=false,yaxis = L"X_t",title = "Pathwise Stiffness Example",
+     xtickfont = font(16, "Ariel"),titlefont = font(20, "Ariel"),
+     ytickfont = font(16, "Ariel"),guidefont = font(18, "Ariel"))
 
 savefig("pathwise_stiffness.png")
 savefig("pathwise_stiffness.pdf")
